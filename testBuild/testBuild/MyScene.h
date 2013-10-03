f305a54f252f40578ec039d0aa5349a53e268e37
Anton Poluboiarynov <apolwork@gmail.com>
2013-10-03 13:14:20 +0300

    Initial Commit

---
+++ testBuild/testBuild/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  testBuild
+//
+
+//  Copyright (c) 2013 Anton Poluboiarynov. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
